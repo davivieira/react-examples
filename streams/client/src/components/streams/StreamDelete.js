0a5533db79e97a3b8a39c8e070629e0ab4b21f2e
davivieira <dvieira0801@gmail.com>
2019-02-10 10:14:58 +0100

    CRUD completed

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React from "react";
+import Modal from "../Modal";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { deleteStream, fetchStream } from "../../actions";
+import history from "../../history";
 
 class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onClickDelete = () => {
+    this.props.deleteStream(this.props.stream.id);
+  };
+
+  renderActions = () => {
+    return (
+      <React.Fragment>
+        <button onClick={this.onClickDelete} className="ui negative button">
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </React.Fragment>
+    );
+  };
+
+  renderContent = () => {
+    if (!this.props.stream) {
+      return "Are you sure you want to delete this stream?";
+    }
+
+    return `Are you sure you want to dele the stream with title: ${
+      this.props.stream.title
+    }`;
+  };
+
   render() {
-    return <div>StreamDelete</div>
+    return (
+      <div>
+        StreamDelete
+        <Modal
+          title="Delete Stream"
+          content={this.renderContent()}
+          actions={this.renderActions()}
+          onDismiss={() => history.push("/")}
+        />
+      </div>
+    );
   }
 }
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { deleteStream, fetchStream }
+)(StreamDelete);
--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
   render() {
-    return <div>StreamShow</div>
+    if (!this.props.stream) {
+      return <div>Loading</div>;
+    }
+
+    const { stream } = this.props;
+
+    return (
+      <div>
+        <h1>{stream.title}</h1>
+        <h5>{stream.description}</h5>
+      </div>
+    );
   }
 }
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamShow);
